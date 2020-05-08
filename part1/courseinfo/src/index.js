3f0ed85eea16e2cd7f1e138e04ddd7dcf691555e
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-05-08 13:52:28 +0530

    courseinfo step2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -7,17 +7,19 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <>
-      <p>
-        {props.part[0]} {props.exercises[0]}
-      </p>
-      <p>
-        {props.part[1]} {props.exercises[1]}
-      </p>
-      <p>
-        {props.part[2]} {props.exercises[2]}
-      </p>
-    </>
+    <div>
+      <Part part={props.part[0]} exercises={props.exercises[0]} />
+      <Part part={props.part[1]} exercises={props.exercises[1]} />
+      <Part part={props.part[2]} exercises={props.exercises[2]} />
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
   );
 };
 
