7ed276df956b918bbcae5a07c83df7f184cf7fe7
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-05-09 12:18:28 +0530

    unicafe step4

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -15,6 +15,26 @@ const Button = (props) => {
   return <button onClick={props.handleClick}>{props.text}</button>;
 };
 
+const Statistics = ({ good, bad, neutral }) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return <p>No feedback given</p>;
+  }
+
+  return (
+    <>
+      <Display text="good" count={good} />
+      <Display text="neutral" count={neutral} />
+      <Display text="bad" count={bad} />
+      <Display text="all" count={good + neutral + bad} />
+      <Display text="average" count={(good - bad) / (good + neutral + bad)} />
+      <Display
+        text="positive"
+        count={(good / (good + neutral + bad)) * 100 + " %"}
+      />
+    </>
+  );
+};
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
@@ -31,15 +51,7 @@ const App = () => {
       ></Button>
       <Button text="bad" handleClick={() => setBad(bad + 1)}></Button>
       <h1>statistics</h1>
-      <Display text="good" count={good} />
-      <Display text="neutral" count={neutral} />
-      <Display text="bad" count={bad} />
-      <Display text="all" count={good + neutral + bad} />
-      <Display text="average" count={(good - bad) / (good + neutral + bad)} />
-      <Display
-        text="positive"
-        count={(good / (good + neutral + bad)) * 100 + " %"}
-      />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
